c00134946c23c69c9f5cf2b9005cd75de3c5ccb9
Karan Rane <65353661+karan685@users.noreply.github.com>
2026-03-13 16:40:50 +0530

    Refactor mac_changer.py to remove unused code
    Removed commented-out code and cleaned up the get_arguments and change_mac functions.

--- mac_changer.py
+++ mac_changer.py
@@ -5,21 +5,11 @@ import optparse
 import re
 
 def get_arguments():
-    # interface = input("interface>")
-    # new_mac = input("New MAC> ")
-
     parser = optparse.OptionParser()
 
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address for the system")
-
-    # not going to use it because we are going to use it directly
-    # interface = options.interface
-    # new_mac = options.new_mac
-
-    #not going to use because it is not be options, arguments are too available out these functions
-    #(options, arguments) = parser.parse_args()
-
+    
     (options, arguments) = parser.parse_args()
 
     if not options.interface:
@@ -33,28 +23,15 @@ def get_arguments():
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-    # subprocess.call("ifconfig " + interface + " down", shell=True)
-    # subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-    # subprocess.call("ifconfig " + interface + " up", shell=True)
-
+    
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-    # print("----------------------------------------------------------------")
-    # #subprocess.call("ifconfig " + interface, shell=True)
-    # result = subprocess.call(["ifconfig", interface])
-    # mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", result)
-    # print(mac_address_search_result.group(0))
-
 def get_current_mac(interface):
     result = subprocess.check_output(["ifconfig", interface])
     ifconfig_result = result.decode("utf-8")
-
-    #print("-------------------------------------------------------------------------")
-    # print(ifconfig_result)
-    #mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
     if mac_address_search_result:
